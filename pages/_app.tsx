553b5e83158191d8ca01cfb589a68d16044208fc
Simon Emanuel Schmid <simon@schmid.io>
2022-10-28 15:51:48 +0200

    Add @self.id/framwork

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "@self.id/framework";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
